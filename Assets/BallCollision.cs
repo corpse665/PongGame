68ec639f3957c2c6dc079425b1ba986aea3a3b1a
akmalibad <akmalibadurrahman8@gmail.com>
2020-04-09 11:52:33 +0700

    first commit

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+       if(other.collider.name == "kanan" || other.collider.name == "kiri"){
+           GetComponent<Transform>().position = new Vector2(0,0);
+       } 
+    }
+}
